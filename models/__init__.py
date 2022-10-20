d4de5e481989cbef7d3ad2a0bb97a7e1b7e5048b
Houda SAFFI <houda.saffi@um6p.ma>
2022-10-20 17:47:28 +0100

    updated models/__init__.py file

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,8 @@ import models.PoinTr
 import models.GRNet
 import models.PCN
 import models.FoldingNet
+import models.PoinTr_inception
+import models.FoldingNet_Resinception_32
+import models.PCN_RESINCEPTION
+import models.PCN_RESINCEPTION_encoder
+import models.PCN_RESINCEPTION_encoder_decoder
